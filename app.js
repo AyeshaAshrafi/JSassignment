be604ba54c20304fe46f366bffdbbcfd7ca8af9e
AyeshaAshrafi <brilliant.ashi100@gmail.com>
2020-06-07 06:16:07 -0700

    ready to commit

---
+++ app.js
@@ -0,0 +1,360 @@
+//chap 1
+
+//task1
+alert("WELCOME EVERYONE!");
+
+//task2
+alert("Error! please enter acorrect password.");
+
+//task3
+alert("Welcome to JS land... \nHappy coding!");
+
+//task4
+alert("Welcome to JS land");
+alert("Happy coding! \nprevent this page from creating additional dialogs");
+
+
+//chap 2
+
+//task1
+var username;
+
+//task2
+var myName = "AYESHA ASHRAFI"
+alert(myName)
+
+//task3
+var message = "Hello World"
+alert(message)
+
+//task4
+var name = "Ali Ahmed"
+var age = 15
+var work = "certified mobile application development"
+alert(name)
+alert("I am" + " " + age + " years old")
+alert(work)
+
+//task5
+var str="PIZZA"
+alert(str+"\n"+str.slice(0,4)+"\n"+str.slice(0,3)+"\n"+str.slice(0,2)+"\n"+str.slice(0,1))
+
+//task6
+var email = "brilliant.ashi100@gmail.com"
+alert("my email is" + " " + email)
+
+//task7
+var book = "A smarter way to learn JavaScript"
+alert("I am trying to learn from the book " + " " + book)
+
+//task8
+var msg = "  Yah!I can write html content through javascript"
+document.write(msg)
+
+//task9
+var chars = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert(chars)
+
+
+
+
+//chap 3
+
+//task1
+var age = "19";
+alert("I am " + " " + age + " " + "years old");
+
+//task2
+var count = " 14";
+ alert("you have visited this website" + " " +count + " " + "times");
+
+//task3
+var birthyear = "2000";
+document.write("<br/>my birthyear is" + " " + birthyear + "<br/>Data type declared is number")
+
+//task4
+var VisitorName ="John Doe";
+var ProductTitle ="T-shirt";
+var quantity ="5";
+document.write("<br>");
+document.write( 
+        VisitorName + " " + "ordered" + " " + quantity + " " + ProductTitle + "(s)" + " " + " on XYZ Clothing store"
+);
+
+//chap 4
+//task1
+var a, b, c;
+//task2
+var $col;
+var _two;
+var one;
+var response;
+var hello;
+//task3
+//var ?two;
+//var GGHHH;
+//var on on;
+//var ? Ywo;
+//var ?COL;
+
+
+//task3
+document.write("<br/><h1/> Rules for naming JS variables")
+document.write("<br/> Variable names can only contain numbers,$ and _ .For example $my_1stVariable <br/> Variables must begin with a letter, $ or _. For example $name, _name or name <br/> Variable names are case sensitive <br/>Variable names should not be JS keywords <br/>")
+
+//chap 5
+
+//task1,2
+var a = 5;
+var b = 3;
+var t = 6;
+var c = a + b;
+var d = a - b;
+var e = a * b;
+var f = t / b;
+var g = a % b;
+document.write("<br/>the sum of" + " " + a + " and " + b + " is " + c)
+document.write("<br/>the difference of" + " " + a + " and " + b + " is " + d)
+document.write("<br/>the product of" + " " + a + " and " + b + " is " + e)
+document.write("<br/>the quotient of" + " " + t + " and " + b + " is " + f)
+document.write("<br/>the modulus of" + " " + a + " and " + b + " is " + g +"<br>")
+
+//task3
+var j = 5;
+document.write("<br/>Value after variable declaration is:undefined")
+document.write("<br/>intial value:" + " " + j)
+k = ++j;
+document.write("<br/>value after increment:" + " " + k)
+l = k + 7;
+document.write("<br/>value after addition:" + " " + l)
+m = --l;
+document.write("<br/>the value after decrement is:" + " " + m)
+var n = m / 3;
+document.write(" <br/>the remainder is:" + " " + n)
+
+//task4
+var h = "600"
+var total = "3000"
+document.write("<br/>total cost to buy 5 tickets to a movie is" + " " + total + " PKR "+"<br>")
+//task5
+document.write("<br>Table of 4 <br>")
+for (var i = 0; i <= 10; i++) {
+
+    document.write("4" + "x" + i + "=" + 4 * i + "<br>")
+}
+
+
+
+//task6
+document.write("<br/> celsius and farenheit converter")
+var cTemp = 28;
+var cToFahr = cTemp * 9 / 5 + 32;
+document.write("<br/>" + cTemp + "C is " + cToFahr + "F")
+var fTemp = 98;
+var fToCel = (fTemp - 32) * 5 / 9;
+document.write("<br/>" + fTemp + "F is " + fToCel + "C" + "<br>")
+
+
+//task7
+var pone = 650
+var ptwo = 100
+var qone = 3
+var qtwo = 7
+var sc = 100
+document.write("<br>Shopping Cart<br>")
+document.write("<br/>Price of item 1 is" + " " + pone)
+document.write("<br/>Quantity of item 1 is" + " " + qone)
+document.write("<br/>Price of item 2 is" + " " + ptwo)
+document.write("<br/>Quantity of item 1 is" + " " + qtwo)
+document.write("<br/>shipping charges" + " " + sc + "<br>")
+
+
+//task8
+var total = 980
+var obt = 804
+document.write("<br/>Marks Sheet <br> ")
+document.write("<br/>total marks" + "=" + total)
+document.write("<br/>obtained marks" + "=" + obt)
+document.write("<br/>percentage" + "=" + (obt / total) * 100 + "% <br>")
+
+
+//task9
+var dollars = 10
+var riyals = 25
+var rs = (dollars * 104.8) + (riyals * 28)
+document.write("<br> Total currency in PKR:" + rs + "<br>")
+
+
+//task10
+var num = 18
+var numn = ((num + 5) * 10) / 12
+console.log(numn)
+
+//task11
+var year = 2016
+var cyear = 1992
+document.write("<br>Age Calculator <br>")
+document.write("<br>Current year:" + year)
+document.write("<br>Birth year:" + cyear)
+document.write("<br>your age is:" + (year - cyear) + "<br>")
+
+//task12
+var radius = 20
+document.write("<br>The Geometrizer<br>")
+document.write("<br>Radius of circle:" + radius)
+document.write("<br>the circumference is:" + (2 * radius * 3.142))
+document.write("<br>the area is:" + (3.142 * 20 * radius) + "<br>")
+
+//task13
+var fav = "chocolate"
+var cage = 20
+var maxage = 65
+var amount = 2
+var cal = (maxage - cage) * 365 * 2
+document.write("<br>The lifetime supply calculator<br>")
+document.write("<br>Favourite snack:" + fav)
+document.write("<br>Current age:" + cage)
+document.write("<br>Maximum age:" + maxage)
+document.write("<br>Amount of snacks per day:" + amount)
+document.write("<br>you will need" + " " + cal + " chocolate to last you until the ripe old age of 65 <br>")
+
+
+// chap 6-9
+
+//task1
+var v = 10
+document.write("<br>Result<br>")
+document.write("<br>The vale of v is:" + v)
+document.write("<br>-------------------------<br>")
+var x = ++v
+document.write("<br>The value of ++v is:" + x)
+document.write("<br>Now the value of v is:" + x + "<br>")
+t = x++
+document.write("<br>The value of v++ is:" + t)
+document.write("<br>Now the value of v is:" + x + "<br>")
+k = --x
+document.write("<br>The value of --v is:" + k)
+document.write("<br>Now the value of v is:" + k + "<br>")
+y = v--
+document.write("<br>The value of v-- is:" + y)
+document.write("<br>Now the value of v is:" + v + "<br>")
+
+//task2
+var a = 2, b = 1;
+document.write("<br>a is:" + a)
+document.write("<br>b is:" + b)
+var result = --a - --b + ++b + b--;
+document.write("<br> result is:" + result)
+document.write("<br>EXPLANATION: <br> --a; is pre-decrement so a decreased from 2 to 1. <br> --a - --b; in this stage 1 is subtracted from the already decreased value of b from 1 to 0 so the calculations of this part is: 1-0. <br> --a - --b + ++b; in this step the value of b which is 0 becomes 1 after pre increment so the calculations are: 1-0+1. <br>--a - --b + ++b + b--; in this step post deccrement function is applied on b so the calculations are: 1- 0 + 1 + 1.")
+
+//task 3
+var usename = prompt("Enter your name");
+alert("Welcome" + " " + usename)
+
+//task-4,5
+var table = +prompt("enter any number");
+if (table < 5) {
+    document.write("<hr>");
+    for (var i = 0; i <= 10; i++) {
+
+        document.write("3" + "x" + i + "=" + 3 * i + "<br>");
+    }
+
+}
+else {
+    document.write("<hr> Default Table <br>");
+    for (var i = 0; i <= 10; i++) {
+
+        document.write("5" + "x" + i + "=" + 5 * i + "<br>");
+    }
+}
+
+//chap 9-11
+
+//task1
+var city = prompt("Enter city name:")
+if (city ==="Karachi") {
+    alert("Welcome to city of lights")
+}
+else {
+    alert("welcome!")
+}
+
+//task2
+var gender = prompt("Enter gender:")
+if (gender == "male") {
+    alert("Good morning Sir!")
+}
+else if (gender == "female") {
+    alert("Good morning Ma'am!")
+
+}
+else {
+    alert("Good morning")
+}
+
+//task3
+var color=prompt("Enter the traffic signal color:")
+if(color=="green"){
+    alert("Move now")
+}
+else if(color=="red"){
+    alert(" must stop ")
+}
+else if(color=="yellow"){
+    alert("Ready to move")
+}
+else{
+    alert("not a color of signal")
+}
+
+
+//task 4
+var fuel = prompt("Enter the amount of fuel remaining in your car ", "current amount");
+if (fuel < 0.25) {
+    alert("Please refill the fuel in your car.");
+}
+else {
+    alert("you've got sufficient amount of fuel, Enjoy your ride!");
+}
+
+
+
+//task 5
+ //var a = 4;
+ //if (++a === 5){
+      //   alert("given condition for variable a is true");
+// }
+ // var b = 82;
+ //if (b++ === 83){
+   //      alert("given condition for variable b is true");
+ //}
+ // var c = 12;
+//  if (c++ === 13){
+  //      alert("condition 1 is true");
+ // }
+  //if(c === 13){
+    //     alert("condition 2 is true");
+  //}
+  //if (++c < 14){
+    //     alert("condition 3 is true");
+ //}
+  //if(c === 14){
+    //     alert("condition 4 is true");  }
+  //var materialCost = 20000;
+  //var laborCost = 2000;
+  //var totalCost = materialCost + laborCost;
+  //if (totalCost === laborCost + materialCost){
+    //     alert("The cost equals");
+  //}
+  //e. if (true){
+
+    //alert("True");
+      //}
+      //alert("False");
+    //}
+      //if("car" < "cat"){
+        // alert("car is smaller than cat");
+     //}
+
+    //No alert message appeared.
